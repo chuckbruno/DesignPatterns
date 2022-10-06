b06cb189daccb8c2b02696bb7977e8384f74a0c1
lulongfei <lulongfei12006@126.com>
2022-10-06 17:04:44 +0800

    conflit: solve conflits

--- Composite/composite.h
+++ Composite/composite.h
@@ -1,5 +1,8 @@
-
 #include <list>
+#include <algorithm>
+#include <iostream>
+#include <string>
+
 
 class File
 {
@@ -67,4 +70,126 @@ public:
 
 protected:
 	int age;
+}
+
+
+class Component
+{
+
+protected:
+	Component* parent_;
+public:
+	virtual ~Component(){}
+	void SetParent(Component* parent)
+	{
+		this->parent_ = parent;
+	}
+	Component* GetParent() const {
+		return this->parent_;
+	}
+
+	virtual void Add(Component* component){}
+	virtual void Remove(Component* component){}
+
+	virtual bool IsComposite()const
+	{
+		return false;
+	}
+
+	virtual std::string Operation() const = 0;
+
+
+};
+
+class Leaf :public Component
+{
+public:
+	std::string Operation() const override
+	{
+		return "Leaf";
+	}
+};
+
+class Composite :public Component
+{
+
+protected:
+	std::list<Component*> children_;
+public:
+	void Add(Component* component) override
+	{
+		this->children_.push_back(component);
+		component->SetParent(this);
+	}
+
+	void Remove(Component* component) override
+	{
+		children_.remove(component);
+		component->SetParent(nullptr);
+	}
+
+	bool IsComposite() const override
+	{
+		return true;
+	}
+
+	std::string Operation() const override
+	{
+		std::string result;
+		for (const Component* c : children_)
+		{
+			if (c == children_.back())
+			{
+				result += c->Operation();
+			}
+			else
+			{
+				result += c->Operation() + "+";
+			}
+		}
+
+		return "Branch(" + result + ")";
+	}
+};
+
+void ClientCode(Component* component)
+{
+	std::cout << "RESULT: " << component->Operation();
+}
+
+
+void ClientCode2(Component* component1, Component* component2)
+{
+	if (component1->IsComposite())
+	{
+		component1->Add(component2);
+	}
+	std::cout << "RESULT: " << component1->Operation();
+}
+
+
+class BaseComponent
+{
+public:
+	virtual void Add(BaseComponent* file) = 0;
+	virtual void Remove(BaseComponent* file) = 0;
+protected:
+	BaseComponent* parent;
+};
+
+class Folder :public BaseComponent {
+
+public:
+	void Add(BaseComponent* file) override
+	{
+
+	}
+
+	void Remove(BaseComponent* file) override
+	{
+
+	}
+
+protected:
+	std::list<BaseComponent*> children;
 };
--- Composite/main.cpp
+++ Composite/main.cpp
@@ -24,57 +24,100 @@
 //}
 
 
-#include <iostream>
-#include <vector>
-using namespace std;
+// #include <iostream>
+// #include <vector>
+// using namespace std;
 
-class PageObject {
-public:
-	virtual void addItem(PageObject a) { }
-	virtual void removeItem() { }
-	virtual void deleteItem(PageObject a) { }
-};
+// class PageObject {
+// public:
+// 	virtual void addItem(PageObject a) { }
+// 	virtual void removeItem() { }
+// 	virtual void deleteItem(PageObject a) { }
+// };
 
-class Page : public PageObject {
-public:
-	void addItem(PageObject a) {
-		cout << "Item added into the page" << endl;
-	}
-	void removeItem() {
-		cout << "Item Removed from page" << endl;
-	}
-	void deleteItem(PageObject a) {
-		cout << "Item Deleted from Page" << endl;
-	}
-};
+// class Page : public PageObject {
+// public:
+// 	void addItem(PageObject a) {
+// 		cout << "Item added into the page" << endl;
+// 	}
+// 	void removeItem() {
+// 		cout << "Item Removed from page" << endl;
+// 	}
+// 	void deleteItem(PageObject a) {
+// 		cout << "Item Deleted from Page" << endl;
+// 	}
+// };
 
-class Copy : public PageObject {
-	vector<PageObject> copyPages;
-public:
-	void AddElement(PageObject a) {
-		copyPages.push_back(a);
-	}
-	void addItem(PageObject a) {
-		cout << "Item added to the copy" << endl;
-	}
-	void removeItem() {
-		cout << "Item removed from the copy" << endl;
-	}
-	void deleteItem(PageObject a) {
-		cout << "Item deleted from the copy";
-	}
-};
+// class Copy : public PageObject {
+// 	vector<PageObject> copyPages;
+// public:
+// 	void AddElement(PageObject a) {
+// 		copyPages.push_back(a);
+// 	}
+// 	void addItem(PageObject a) {
+// 		cout << "Item added to the copy" << endl;
+// 	}
+// 	void removeItem() {
+// 		cout << "Item removed from the copy" << endl;
+// 	}
+// 	void deleteItem(PageObject a) {
+// 		cout << "Item deleted from the copy";
+// 	}
+// };
 
-int main() {
-	Page p1;
-	Page p2;
+// int main() {
+// 	Page p1;
+// 	Page p2;
 
-	std::cout << sizeof(p1) << std::endl;
-	Copy myCopy;
-	myCopy.AddElement(p1);
-	myCopy.AddElement(p2);
-	myCopy.addItem(p1);
-	p1.addItem(p2);
-	myCopy.removeItem();
-	p2.removeItem();
+// 	std::cout << sizeof(p1) << std::endl;
+// 	Copy myCopy;
+// 	myCopy.AddElement(p1);
+// 	myCopy.AddElement(p2);
+// 	myCopy.addItem(p1);
+// 	p1.addItem(p2);
+// 	myCopy.removeItem();
+// 	p2.removeItem();
+
+#include "composite.h"
+
+int main()
+{
+	Component* simple = new Leaf;
+	std::cout << "Client: I've got a simple component:\n";
+	ClientCode(simple);
+	std::cout << "\n\n";
+
+	Component* tree = new Composite;
+	Component* branch1 = new Composite;
+
+	Component* leaf_1 = new Leaf;
+	Component* leaf_2 = new Leaf;
+	Component* leaf_3 = new Leaf;
+	branch1->Add(leaf_1);
+	branch1->Add(leaf_2);
+
+	Component* branch2 = new Composite;
+	branch2->Add(leaf_3);
+	tree->Add(branch1);
+	tree->Add(branch2);
+
+	std::cout << "Client: Now I've got a composite tree:\n";
+	ClientCode(tree);
+	std::cout << "\n\n";
+
+	std::cout << "Client: I don't need to check the components classes even when managing the tree.\n";
+	ClientCode2(tree, simple);
+	std::cout << "\n";
+
+	delete simple;
+	delete tree;
+	delete branch1;
+	delete branch2;
+	delete leaf_1;
+	delete leaf_2;
+	delete leaf_3;
+
+	return 0;
+
+>>>>>>> 7048a4c (feat: update codes)
 }
